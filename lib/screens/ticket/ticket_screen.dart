bd620d8d5704e5e4b47c14fd0793b78725359f00
Praju <prajaktagai11@gmail.com>
2024-07-22 18:47:15 +0530

    first commit

--- lib/screens/ticket/ticket_screen.dart
+++ lib/screens/ticket/ticket_screen.dart
@@ -1,3 +1,4 @@
+import 'package:barcode_widget/barcode_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:ticket_book/base/res/media.dart';
 import 'package:ticket_book/base/res/styles/app_styles.dart';
@@ -17,122 +18,194 @@ class TicketScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppStyles.bgColor,
-      body: ListView(
-        padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+      body: Stack(
         children: [
-          const SizedBox(
-            height: 40,
-          ),
-          Text(
-            "Tickets",
-            style: AppStyles.headlineStyle1,
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          const AppTicketTabs(
-            text1: 'Upcoming',
-            text2: 'Previous',
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Container(
-            padding: const EdgeInsets.only(left: 16),
-            child: TicketView(
-              ticket: ticketList[0],
-              isColor: true,
-            ),
-          ),
-          const SizedBox(
-            height: 1,
-          ),
-          Container(
-            margin: const EdgeInsets.symmetric(horizontal: 15),
-            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
-            color: AppStyles.ticketColor,
-            child:  Column(
-              children: [
-                const Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          ListView(
+            padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+            children: [
+              const SizedBox(
+                height: 40,
+              ),
+              Text(
+                "Tickets",
+                style: AppStyles.headlineStyle1,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const AppTicketTabs(
+                text1: 'Upcoming',
+                text2: 'Previous',
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                padding: const EdgeInsets.only(left: 16),
+                child: TicketView(
+                  ticket: ticketList[0],
+                  isColor: true,
+                ),
+              ),
+              const SizedBox(
+                height: 1,
+              ),
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 15),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+                color: AppStyles.ticketColor,
+                child: Column(
                   children: [
-                    AppColumnTextLayout(
-                      topText: "Flutter DB",
-                      bottomText: "Passenger",
-                      alignment: CrossAxisAlignment.start,
-                      isColor: true,
-                    ),
-                    AppColumnTextLayout(
-                      topText: "52245455224",
-                      bottomText: "Passport",
-                      alignment: CrossAxisAlignment.end,
-                      isColor: true,
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                AppLayoutBuilder(randomDivider: 15, width: 5, isColor: true),
-                SizedBox(
-                  height: 20,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    AppColumnTextLayout(
-                      topText: "24645 232423523  ",
-                      bottomText: "Number of E-ticket",
-                      alignment: CrossAxisAlignment.start,
-                      isColor: true,
-                    ),
-                    AppColumnTextLayout(
-                      topText: "B3465456",
-                      bottomText: "Booking code",
-                      alignment: CrossAxisAlignment.end,
-                      isColor: true,
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                AppLayoutBuilder(randomDivider: 15, width: 5, isColor: true),
-                SizedBox(
-                  height: 20,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                    const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Row(
-                          children: [
-                            Image.asset(AppMedia.visaCard),
-                            TextStyleThird(text: "***2463",isColor: true,),
-                          ],
+                        AppColumnTextLayout(
+                          topText: "Flutter DB",
+                          bottomText: "Passenger",
+                          alignment: CrossAxisAlignment.start,
+                          isColor: true,
                         ),
-                         SizedBox(
-                          height: 5,
-                        ),
-                        TextStyleFourth(
-                          text: "Payment method",
+                        AppColumnTextLayout(
+                          topText: "52245455224",
+                          bottomText: "Passport",
+                          alignment: CrossAxisAlignment.end,
                           isColor: true,
                         ),
                       ],
                     ),
-                    AppColumnTextLayout(
-                      topText: "\$249.99",
-                      bottomText: "Price",
-                      alignment: CrossAxisAlignment.end,
-                      isColor: true,
+                    SizedBox(
+                      height: 20,
+                    ),
+                    AppLayoutBuilder(
+                        randomDivider: 15, width: 5, isColor: true),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        AppColumnTextLayout(
+                          topText: "24645 232423523  ",
+                          bottomText: "Number of E-ticket",
+                          alignment: CrossAxisAlignment.start,
+                          isColor: true,
+                        ),
+                        AppColumnTextLayout(
+                          topText: "B3465456",
+                          bottomText: "Booking code",
+                          alignment: CrossAxisAlignment.end,
+                          isColor: true,
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    AppLayoutBuilder(
+                        randomDivider: 15, width: 5, isColor: true),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              children: [
+                                Image.asset(
+                                  AppMedia.visaCard,
+                                  scale: 11,
+                                ),
+                                TextStyleThird(
+                                  text: " *** 2463",
+                                  isColor: true,
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            TextStyleFourth(
+                              text: "Payment method",
+                              isColor: true,
+                            ),
+                          ],
+                        ),
+                        AppColumnTextLayout(
+                          topText: "\$249.99",
+                          bottomText: "Price",
+                          alignment: CrossAxisAlignment.end,
+                          isColor: true,
+                        ),
+                      ],
                     ),
                   ],
                 ),
-              ],
-            ),
-          )
+              ),
+              const SizedBox(
+                height: 1,
+              ),
+              Container(
+                  margin: EdgeInsets.symmetric(horizontal: 15),
+                  padding: const EdgeInsets.symmetric(vertical: 20),
+                  decoration: BoxDecoration(
+                      color: AppStyles.ticketColor,
+                      borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(21),
+                          bottomRight: Radius.circular(21))),
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 15),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(21),
+                      child: BarcodeWidget(
+                          height: 70,
+                          width: double.infinity,
+                          drawText: false,
+                          color: AppStyles.textColor,
+                          data: "https://www.google.com",
+                          barcode: Barcode.code128()),
+                    ),
+                  )),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                padding: const EdgeInsets.only(left: 16),
+                child: TicketView(
+                  ticket: ticketList[0],
+                ),
+              ),
+            ],
+          ),
+          Positioned(
+              left: 22,
+              top: 250,
+              child: Container(
+                padding: const EdgeInsets.all(3),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,border: Border.all(width: 2)
+                ),
+                child: CircleAvatar(
+                  maxRadius: 4,
+                  backgroundColor: AppStyles.textColor,
+                ),
+              )),
+          Positioned(
+              right: 22,
+              top: 250,
+              child: Container(
+                padding: const EdgeInsets.all(3),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,border: Border.all(width: 2)
+                ),
+                child: CircleAvatar(
+                  maxRadius: 4,
+                  backgroundColor: AppStyles.textColor,
+                ),
+              )),
         ],
       ),
     );
